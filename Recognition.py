0197f6bd0e0417e09ece20f8b562bf738917f18c
RobD8888 <122328034+RobD8888@users.noreply.github.com>
2023-04-22 17:27:27 -0400

    Add files via upload

---
+++ Recognition.py
@@ -0,0 +1,53 @@
+import threading
+import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+counter = 0
+
+face_match = False
+
+reference_img = cv2.imread("Robert.jpg")
+
+def check_face(frame):
+    global face_match
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    print(f"Number of faces detected: {len(faces)}")
+    if len(faces) > 0:
+        face_match = True
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        print("Rectangles drawn on detected faces.")
+    else:
+        face_match = False
+    print("check_face function called.")
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
+
+        if face_match:
+            cv2.putText(frame, "Robert", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow("video", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
